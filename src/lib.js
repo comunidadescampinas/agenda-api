c41a9c139b717f0e4bc402a3d1e9b2acfb92088b
Filipe Oliveira <contato@fmoliveira.com.br>
2017-08-28 23:52:51 -0300

    Normalizar resultado da API.

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,24 @@
 import 'isomorphic-fetch'
+import { normalize, schema } from 'normalizr'
 
 import comunidades from '../data/comunidades.json'
 
 const BASE_URL = 'https://api.meetup.com'
 const API_KEY = process.env.MEETUP_API_KEY
 
+const group = new schema.Entity('groups')
+
+const venue = new schema.Entity('venues')
+
+const event = new schema.Entity('events', {
+  group: group,
+  venue: venue
+})
+
+const mapToJson = res => res.json()
+const normalizeEvents = res => normalize(res, [event])
+
 export function listarEventos (comunidade) {
   const url = `${BASE_URL}/${comunidade}/events?key=${API_KEY}`
-  return fetch(url).then(res => res.json())
+  return fetch(url).then(mapToJson).then(normalizeEvents)
 }
