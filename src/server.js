2bcc62fecc7b7d4e8875653897e7a24ef1deefb7
Victor Perin <victor.perin@ingaia.com.br>
2017-08-31 16:17:35 -0300

    refactor(server/wrapResponse): using promises for legibility

--- src/server.js
+++ src/server.js
@@ -9,19 +9,16 @@ const PORT = process.env.PORT || 3001
 const app = new Koa()
 const router = new Router()
 
-async function wrapResponse (fn) {
-  try {
-    return {
+const wrapResponse = (fn) =>
+  fn()
+    .then( (body) => ({
       status: 'success',
-      body: await fn()
-    }
-  } catch (err) {
-    return {
+      body
+    }) )
+    .catch( (error) => ({
       status: 'error',
       message: err.message
-    }
-  }
-}
+    }) )
 
 router.get('/eventos', async (ctx, next) => {
   ctx.body = await wrapResponse(listarEventosTodasComunidades)
